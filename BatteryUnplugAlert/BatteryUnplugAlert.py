6e25e88816c69a16ea74011b52d9dba45892da58
Danny Hayworth <DannyHayworth@wentworth.school.nz>
2026-08-17 12:45:18 +1200

    Removed time from requirements since it is built in. Added custom frequency of checks and other improvements to the program

--- BatteryUnplugAlert/BatteryUnplugAlert.py
+++ BatteryUnplugAlert/BatteryUnplugAlert.py
@@ -1,27 +1,37 @@
 # Gives a notification when your charger is unplugged
 # Run pythonw BatterAlert.py to run in the background
 # Run taskkill /IM pythonw.exe /F to end the program
-
-# Import required functions
-import psutil
-import time
+import psutil, time
 from plyer import notification
 
 unplugged = False
+accepted = False
+
+while not accepted:
+    checkFrequency = input("How often the status of your charger will be checked in seconds: ")
+    try:
+        checkFrequency = float(checkFrequency)
+        if checkFrequency > 0:
+            print(f"Your charger will now be checked every {checkFrequency} seconds.")
+            accepted = True
+        else:
+            print("Number must be positive")
+    except ValueError:
+        print("Please enter a number")
 
 while True:
-          time.sleep(12)
-          # Check if it is plugged in
-          if(unplugged == False and psutil.sensors_battery().power_plugged == False):
-                    # make sure it doesn't send the notification every 12 seconds!
-                    unplugged = True
-                    #notify user
-                    notification.notify(
-                              title = 'Warning!', 
-                              message = "Someone has unpluged your charger",
-                              app_icon = None,
-                              timeout = 10,
-                    )
-          # If it is plugged in again allow the notification to be send again
-          if(psutil.sensors_battery().power_plugged == True):
-                    unplugged = False
+    time.sleep(checkFrequency)
+    # Check if it is plugged in
+    if unplugged == False and psutil.sensors_battery().power_plugged == False:
+        # make sure it doesn't send the notification every 12 seconds!
+        unplugged = True
+        #notify user
+        notification.notify(
+            title = 'Warning!',
+            message = "Someone has unplugged your charger",
+            app_icon = None,
+            timeout = 10,
+        )
+    # If it is plugged in again allow the notification to be send again
+    if psutil.sensors_battery().power_plugged:
+        unplugged = False
